c48c3f27ee485210e774e8edc825d8820c4da9b8
Yuriy Homyakov <yuriy.homyakov@gmail.com>
2018-10-03 20:53:49 +0300

    add assets updates management functions

--- jwallet_updates/app.py
+++ jwallet_updates/app.py
@@ -1,14 +1,14 @@
 import os
 
+from git import Repo
 import asyncio
 from aiohttp import web
+# from aiohttp_swagger import setup_swagger
 
 
-SOMEVAR = os.getenv('JWALLET_UPDATES___')
-
-STATUS_UP_TO_DATE = 'UP_TO_DATE'
-STATUS_UPDATE_AVAILABLE = 'UPDATE_AVAILABLE'
-STATUS_UPDATE_REQUIRED = 'UPDATE_REQUIRED'
+ASSETS_REPO_PATH = os.path.join(os.path.dirname(__file__, 'assets'))
+ASSETS_IDS_FILE = os.path.join(ASSETS_REPO_PATH, 'assets_ids.json')
+ACTUAL_VERSION_FILE = os.path.join(os.path.dirname(__file__, 'actual_versions.json'))
 
 
 def get_actual_assets():
@@ -20,18 +20,41 @@ def get_actual_assets():
 
 
 routes = web.RouteTableDef()
+routes.static('/v1/assets/', ASSETS_REPO_PATH)
 
 
-@routes.get('/v1/{platform}/{locale}/{version}/status')
+@routes.get('/v1/{platform}/{version}/status')
 async def get_version_status(request):
     """
     Checks moblie app version status: up to date, update available or update required
     """
-    status = {
-        'status': '',
-        'updateDescription': '',
-    }
+    version = request.match_info['version']
+    platform = request.match_info['platform']
+    actual_versions = request.app['versions']
+    if version not in actual_versions[platform]:  
+        status = {
+            'status': STATUS_UPDATE_REQUIRED,
+        }
+    else:
+        status = {
+            'status': STATUS_UP_TO_DATE,
+        }
     return web.json_response(status)
+
+
+@routes.post('/v1/check_updates')
+async def check_assests_updates(request):
+    """
+    Checks assets versions and returns assets IDs that needds updates
+    """
+    app_assets = await request.json()
+    actual_assets = get_actual_assets()
+    result = []
+    for asset in app_assets:
+        actual_version = actual_assets.get(asset['id'])
+        if actual_version is not None and actual_version != asset['version']:
+            result.append(asset['id'])
+    return web.json_response(result)
 
 
 def make_app():
--- jwallet_updates/tests.py
+++ jwallet_updates/tests.py
@@ -1,7 +1,84 @@
 import os
+import shutil
 
+import git
 import pytest
 
 
-def test_get_version_status():
-    assert False
+repo_path = '/tmp/jwallet_assets_test_repo'
+os.environ['JWALLET_ASSETS_REPO_PATH'] = repo_path
+
+
+from jwallet_assets.app import make_app
+
+
+@pytest.fixture
+def assets_repo():
+    if os.path.exists(repo_path):
+        shutil.rmtree(repo_path)
+    repo = git.Repo.init(repo_path)
+    with open(os.path.join(repo_path, 'file1.txt'), 'w') as f:
+        f.write('1\n2\n')
+    repo.git.add('file1.txt')
+    with open(os.path.join(repo_path, 'file2.txt'), 'w') as f:
+        f.write('3\n4\n')
+    repo.git.add('file2.txt')
+    os.mkdir(os.path.join(repo_path, 'dir_one'))
+    with open(os.path.join(repo_path, 'dir_one', 'file3.txt'), 'w') as f:
+        f.write('5\n6\n')
+    repo.git.add('dir_one/file3.txt')
+    with open(os.path.join(repo_path, 'dir_one', 'file4.txt'), 'w') as f:
+        f.write('7\n8\n')
+    repo.git.add('dir_one/file4.txt')
+    repo.index.commit('start')
+    yield repo
+    shutil.rmtree(repo_path)
+
+
+async def test_get_updates_blank(aiohttp_client, loop, assets_repo):
+    cli = await aiohttp_client(make_app())
+    res = await cli.post('/v1/check_updates', json=[{'id': 'no_such_file.txt', 'version': 'abc'}])
+    assert res.status == 200
+    assert await res.json() == []
+
+
+async def test_get_updates(aiohttp_client, loop, assets_repo):
+    cli = await aiohttp_client(make_app())
+    repo_items = list(assets_repo.head.commit.tree.traverse())
+    res = await cli.post('/v1/check_updates',
+                         json=[
+                            {'id': 'dir_one/file4.txt', 'version': repo_items[-1].hexsha[:6]},
+                            {'id': 'dir_one/file3.txt', 'version': 'bca'},
+                            {'id': 'file1.txt', 'version': 'abc'},
+                          ]
+                        )
+    assert res.status == 200
+    assert await res.json() == ['dir_one/file3.txt', 'file1.txt']
+
+
+async def test_get_updates_change_files(aiohttp_client, loop, assets_repo):
+    cli = await aiohttp_client(make_app())
+    repo_items = list(assets_repo.head.commit.tree.traverse())
+    current_versions = [
+                            {'id': 'dir_one/file4.txt', 'version': repo_items[-1].hexsha[:6]},
+                            {'id': 'dir_one/file3.txt', 'version': repo_items[-2].hexsha[:6]},
+                            {'id': 'file1.txt', 'version': repo_items[1].hexsha[:6]},
+                          ]
+    res = await cli.post('/v1/check_updates', json=current_versions)
+    assert res.status == 200
+    assert await res.json() == []
+    fname = os.path.join(assets_repo.working_tree_dir, 'file1.txt')
+    with open(fname, 'a') as f:
+        f.write('z')
+    
+    res = await cli.post('/v1/check_updates', json=current_versions)
+    assert res.status == 200
+    assert await res.json() == []
+    
+    assets_repo.git.add('file1.txt')
+    assets_repo.index.commit('chage file1.txt')
+
+    res = await cli.post('/v1/check_updates', json=current_versions)
+    assert res.status == 200
+    assert await res.json() == ['file1.txt']
+
